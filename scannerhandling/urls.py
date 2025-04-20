5854c58b7619092d35d4a3f12c4a2cf21fbad774
vishnusk2004 <kumarsravan2004@gmail.com>
2025-04-20 18:48:06 +0530

    Logic to send mail

--- scannerhandling/urls.py
+++ scannerhandling/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.about, name="about"),  # ✅ Shows the About page first
@@ -10,5 +11,9 @@ urlpatterns = [
     path("logout/", views.logout_user, name="logout"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("report/", views.report, name="report"),
-]
 
+    path('reset-password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset-password-sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset-password-complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
